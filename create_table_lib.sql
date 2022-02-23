59163d1044737bd3c8d408f632461f2c14f11c83
yashmishra-tudip <99390230+yashmishra-tudip@users.noreply.github.com>
2022-02-23 22:53:12 +0530

    Add files via upload

---
+++ create_table_lib.sql
@@ -0,0 +1,18 @@
+use lib_1;
+
+insert into Lib 
+(Book_name, Author_name, date_of_issue, enrollment_date) 
+values 
+('Destiny', 'J.K. Rowlling', '2022-02-22', null);
+
+insert into Lib 
+(Book_name, Author_name, date_of_issue, enrollment_date) 
+values 
+('DestinyA', 'J.K. RowllingA', '2022-02-23', null),
+('DestinyB', 'J.K. RowllingB', '2022-02-24', null),
+('DestinyC', 'J.K. RowllingC', '2022-02-25', null),
+('DestinyD', 'J.K. RowllingD', '2022-02-26', null),
+('DestinyE', 'J.K. RowllingE', '2022-02-27', null),
+('DestinyF', 'J.K. RowllingF', '2022-02-28', null),
+('DestinyG', 'J.K. RowllingG', '2022-02-21', null),
+('DestinyH', 'J.K. RowllingH', '2022-04-22', null);
